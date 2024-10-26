b4fe34bfd52fe2ebd8d01c299d46d6553b0270d6
amoghkokari <amoghsrcm@gmail.com>
2024-10-26 10:29:34 -0400

    mc

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from app.api.v1.endpoints.health import router as health_router
 from app.api.v1.endpoints.generate_response import router as generate_response_router
 
 # Initialize FastAPI application
-app = FastAPI()
+fast_app = FastAPI()
 
 # Include routers for different endpoints
-app.include_router(health_router)
-app.include_router(generate_response_router)
+fast_app.include_router(health_router)
+fast_app.include_router(generate_response_router)
